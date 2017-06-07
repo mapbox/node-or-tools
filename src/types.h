a5e005dafc3d2c64d36c6849ac9e61af556734ff
Daniel J. Hofmann <daniel@trvx.org>
2017-06-07 15:17:57 +0200

    Simplify segfaulting code

--- src/types.h
+++ src/types.h
@@ -10,17 +10,9 @@
 
 // Convenience types. Sshould be good enough for most of TSP / VRP related problems.
 
-// Newtype via phantom type scheme. Allows type system to distinguish between tagged types.
-// Note: NewType<T, struct Tag0>::Type != NewType<T, struct Tag1>::Type
-template <typename T, typename Tag> struct NewType {
-  struct Type : T {
-    using T::T;
-  };
-};
-
-using CostMatrix = NewType<Matrix<std::int32_t>, struct CostMatrixTag>::Type;
-using DurationMatrix = NewType<Matrix<std::int32_t>, struct DurationMatrixTag>::Type;
-using DemandMatrix = NewType<Matrix<std::int32_t>, struct DemandMatrixTag>::Type;
+using CostMatrix = Matrix<std::int32_t>;
+using DurationMatrix = Matrix<std::int32_t>;
+using DemandMatrix = Matrix<std::int32_t>;
 
 struct Interval {
   Interval() : start{0}, stop{0} {}
@@ -34,7 +26,7 @@ struct Interval {
   std::int32_t stop;
 };
 
-using TimeWindows = NewType<Vector<Interval>, struct TimeWindowsTag>::Type;
+using TimeWindows = Vector<Interval>;
 
 namespace ort = operations_research;
 
@@ -66,48 +58,7 @@ using RouteLocks = std::vector<LockChain>;
 // e.g. pickups: [1, 2], deliveries: [5, 6] means
 //  - pick up at node 1 and deliver to node 5
 //  - pick up at node 2 and deliver to node 6
-using Pickups = NewType<Vector<NodeIndex>, struct PickupsTag>::Type;
-using Deliveries = NewType<Vector<NodeIndex>, struct DeliveriesTag>::Type;
-
-// Bytes in our type used for internal caching
-
-template <typename T> struct Bytes;
-
-template <> struct Bytes<CostMatrix> {
-  std::int32_t operator()(const CostMatrix& v) const { return v.size() * sizeof(CostMatrix::Value); }
-};
-
-template <> struct Bytes<DurationMatrix> {
-  std::int32_t operator()(const DurationMatrix& v) const { return v.size() * sizeof(DurationMatrix::Value); }
-};
-
-template <> struct Bytes<DemandMatrix> {
-  std::int32_t operator()(const DemandMatrix& v) const { return v.size() * sizeof(DemandMatrix::Value); }
-};
-
-template <> struct Bytes<TimeWindows> {
-  std::int32_t operator()(const TimeWindows& v) const { return v.size() * sizeof(TimeWindows::Value); }
-};
-
-template <> struct Bytes<RouteLocks> {
-  std::int32_t operator()(const RouteLocks& v) const {
-    std::int32_t bytes = 0;
-
-    for (const auto& lockChain : v)
-      bytes += lockChain.size() * sizeof(LockChain::value_type);
-
-    return bytes;
-  }
-};
-
-template <> struct Bytes<Pickups> {
-  std::int32_t operator()(const Pickups& v) const { return v.size() * sizeof(Pickups::Value); }
-};
-
-template <> struct Bytes<Deliveries> {
-  std::int32_t operator()(const Deliveries& v) const { return v.size() * sizeof(Deliveries::Value); }
-};
-
-template <typename T> std::int32_t getBytes(const T& v) { return Bytes<T>{}(v); }
+using Pickups = Vector<NodeIndex>;
+using Deliveries = Vector<NodeIndex>;
 
 #endif
